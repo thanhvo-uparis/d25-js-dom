e4ccd9f5570d46d954dceef2fbf960d196b992f3
Thanh VO <thanhvo.uparis@gmail.com>
2025-07-12 13:04:16 +0100

    Count the selected number of checkboxes

--- main.js
+++ main.js
@@ -1,3 +1,31 @@
 const $ = document.querySelector.bind(document);
-const $$ = document.querySelector.bind(document);
+const $$ = document.querySelectorAll.bind(document);
 
+const countSelected = $("#checked-count");
+console.log(countSelected);
+
+const checkAll = $("#check-all");
+const checkRows = $$(".row-checkbox");
+console.log(checkRows);
+
+function uploadDisplay(arr) {
+    // Đếm số checkbox được chọn
+    const updateCheckedCount = () => {
+        let checkedCount = 0;
+        arr.forEach(cb => {
+            if (cb.checked) {
+                checkedCount += 1;
+            }
+        })
+        countSelected.textContent = `${checkedCount} Selected`;
+    }
+
+    // Lắng nghe sử kiện mỗi lần checkbox thay đổi
+    checkRows.forEach(cb => {
+        cb.addEventListener("change", updateCheckedCount);
+    })
+    //render khởi tạo lúc chưa ai chọn gì
+    updateCheckedCount()
+}
+
+uploadDisplay(checkRows);
