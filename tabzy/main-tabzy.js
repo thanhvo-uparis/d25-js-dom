c8d50fdb6e60f265603f8370571fe9a5a784fc0e
thanhvo <thanhvo.uparis@gmail.com>
2025-07-20 00:37:26 +0100

    @tabs: hide active tab earlier when clicking

--- tabzy/main-tabzy.js
+++ tabzy/main-tabzy.js
@@ -1,12 +1,17 @@
 const $ = document.querySelector.bind(document);
 const $$ = document.querySelectorAll.bind(document);
-
 const tabs = document.querySelectorAll(".tabs-container");
+
 tabs.forEach(tab => {
     const listItems = tab.querySelectorAll(".btn-item");
+    if (listItems.length) {
+        listItems[0].classList.add("active");
+    }
     listItems.forEach(item => {
-        item.onclick = () => {
-            item.classList.add("show");
+        item.onclick = function() {
+            const itemActivate = tab.querySelector(".active");
+            itemActivate.classList.remove("active");
+            this.classList.add("active");
         }
     })
 })
