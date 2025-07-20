91bfadb25b81e737d9a9b2789571734b8cc56a0a
thanhvo <thanhvo.uparis@gmail.com>
2025-07-20 01:46:32 +0100

    @tabs: tối ưu code, tách hàm riêng

--- tabzy/main-tabzy.js
+++ tabzy/main-tabzy.js
@@ -6,29 +6,23 @@ tabs.forEach(tab => {
     const listItems = tab.querySelectorAll(".btn-item");
     const listContents = tab.querySelectorAll(".tab-content");
 
-    if (listItems.length) {
+    if (listItems.length && listContents.length) {
         listItems[0].classList.add("active");
         listContents[0].classList.add("active");
     }
+    const activateTab = (tabIndex) => {
+        if (tabIndex < 0 || (tabIndex > listItems.length - 1)) return;
+        const itemActivate = tab.querySelector(".btn-item.active");
+        itemActivate.classList.remove("active");
+        const contentActivate = tab.querySelector(".tab-content.active");
+        contentActivate.classList.remove("active");
+        
+        listItems[tabIndex].classList.add("active");
+        listContents[tabIndex].classList.add("active");
+    }
+    
     listItems.forEach((item, index) => {
-        item.onclick = function() {
-            const itemActivate = tab.querySelector(".btn-item.active");
-            itemActivate.classList.remove("active");
-            this.classList.add("active");
-
-            const contentActivate = tab.querySelector(".tab-content.active");
-            contentActivate.classList.remove("active");
-            listContents[index].classList.add("active");
-        }
+        item.addEventListener("click", () => activateTab(index));
     })
 
-    document.body.addEventListener("keydown", function(event){
-        listItems.forEach(item => {
-            const dataTab = item.dataset.tab;
-            if (event.key === dataTab) {
-                console.log(item);
-            }
-        })
-    })
 })
-
