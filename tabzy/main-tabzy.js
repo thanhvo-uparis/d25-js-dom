466ea66c542dfb71d176f5cce5dffadc57ca8152
thanhvo <thanhvo.uparis@gmail.com>
2025-07-20 01:26:39 +0100

    @tabs: ẩn/hiện nội dung

--- tabzy/main-tabzy.js
+++ tabzy/main-tabzy.js
@@ -4,15 +4,31 @@ const tabs = document.querySelectorAll(".tabs-container");
 
 tabs.forEach(tab => {
     const listItems = tab.querySelectorAll(".btn-item");
+    const listContents = tab.querySelectorAll(".tab-content");
+
     if (listItems.length) {
         listItems[0].classList.add("active");
+        listContents[0].classList.add("active");
     }
-    listItems.forEach(item => {
+    listItems.forEach((item, index) => {
         item.onclick = function() {
-            const itemActivate = tab.querySelector(".active");
+            const itemActivate = tab.querySelector(".btn-item.active");
             itemActivate.classList.remove("active");
             this.classList.add("active");
+
+            const contentActivate = tab.querySelector(".tab-content.active");
+            contentActivate.classList.remove("active");
+            listContents[index].classList.add("active");
         }
+    })
+
+    document.body.addEventListener("keydown", function(event){
+        listItems.forEach(item => {
+            const dataTab = item.dataset.tab;
+            if (event.key === dataTab) {
+                console.log(item);
+            }
+        })
     })
 })
 
