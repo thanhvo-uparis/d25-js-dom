99f9d847c8b844858f11c42498ed2fe56a85da38
thanhvo <thanhvo.uparis@gmail.com>
2025-07-21 17:46:34 +0100

    optimise le code dans commit precedent

--- slideshow/main-slideshow.js
+++ slideshow/main-slideshow.js
@@ -2,15 +2,17 @@ const slidesList = document.querySelectorAll(".slide");
 const btnList = document.querySelectorAll(".btn-action");
 const pagiDot =document.querySelector(".pagination");
 const dotsList = document.querySelectorAll(".dot");
+const inner = document.querySelector(".slide");
 let currentSlide = 0;
 
-function showSlide(index) {
+function uploadSlide(index) {
+    dotsList.forEach(slide => slide.classList.remove("active"));
+    dotsList[index].classList.add("active");
     slidesList.forEach(slide => slide.classList.remove("active"));
     slidesList[index].classList.add("active");
 }
 
 if (slidesList.length > 0 && dotsList.length > 0) {
-    showSlide(currentSlide);
     uploadSlide(currentSlide);
 }
 
@@ -18,11 +20,9 @@ btnList.forEach(btn => {
     btn.addEventListener("click", function() {
         if (btn.classList.contains("next")) {
             currentSlide = (currentSlide + 1) % slidesList.length;
-            showSlide(currentSlide);
             uploadSlide(currentSlide);
         } else if (btn.classList.contains("prev")) {
             currentSlide = ((currentSlide - 1 + slidesList.length) % slidesList.length);
-            showSlide(currentSlide);
             uploadSlide(currentSlide);
         }
     }) 
@@ -32,15 +32,7 @@ dotsList.forEach((dot, index) => {
     dot.addEventListener("click", function(event) {
         const dotSelected = event.target.closest(".dot");
         if (dotSelected) {
-            showSlide(index);
             uploadSlide(index);
         }
-        
     })
 })
-
-// upload current slide for dot
-function uploadSlide(index) {
-    dotsList.forEach(slide => slide.classList.remove("active"));
-    dotsList[index].classList.add("active");
-}
