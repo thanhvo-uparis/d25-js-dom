f09871923050f153284902e85c69e1400ad48b20
thanhvo <thanhvo.uparis@gmail.com>
2025-07-21 17:17:13 +0100

    upload current slide for dot(pagination)

--- slideshow/main-slideshow.js
+++ slideshow/main-slideshow.js
@@ -1,5 +1,7 @@
 const slidesList = document.querySelectorAll(".slide");
 const btnList = document.querySelectorAll(".btn-action");
+const pagiDot =document.querySelector(".pagination");
+const dotsList = document.querySelectorAll(".dot");
 let currentSlide = 0;
 
 function showSlide(index) {
@@ -7,8 +9,9 @@ function showSlide(index) {
     slidesList[index].classList.add("active");
 }
 
-if (slidesList.length > 0) {
+if (slidesList.length > 0 && dotsList.length > 0) {
     showSlide(currentSlide);
+    uploadSlide(currentSlide);
 }
 
 btnList.forEach(btn => {
@@ -16,9 +19,28 @@ btnList.forEach(btn => {
         if (btn.classList.contains("next")) {
             currentSlide = (currentSlide + 1) % slidesList.length;
             showSlide(currentSlide);
+            uploadSlide(currentSlide);
         } else if (btn.classList.contains("prev")) {
             currentSlide = ((currentSlide - 1 + slidesList.length) % slidesList.length);
             showSlide(currentSlide);
+            uploadSlide(currentSlide);
         }
     }) 
+});
+
+dotsList.forEach((dot, index) => {
+    dot.addEventListener("click", function(event) {
+        const dotSelected = event.target.closest(".dot");
+        if (dotSelected) {
+            showSlide(index);
+            uploadSlide(index);
+        }
+        
+    })
 })
+
+// upload current slide for dot
+function uploadSlide(index) {
+    dotsList.forEach(slide => slide.classList.remove("active"));
+    dotsList[index].classList.add("active");
+}
